1a9776c9c1eda5bbef4f31ff8af1437216e23f68
Billy Salinas <wsalinas315@gmail.com>
2022-11-05 16:39:35 -0500

    set up app.listen, calculateResult function, app.get for /result, result.js module and send/getEquation functions.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,7 @@
 $(document).ready(onReady);
 
 // declare global variables
-let operator;
+let eqOperator;
 
 // onReady function 
 function onReady(){
@@ -13,7 +13,7 @@ function onReady(){
 // function to assign string value of operator the pressed button
 function selectOperator(){
     $('.math').css('opacity','0.4');
-	operator = $(this).text();
+	eqOperator = $(this).text();
     $(this).css('opacity', '1');
     console.log('This button was clicked:', operator);
 }
@@ -21,13 +21,42 @@ function selectOperator(){
 // sendEquation function to post equation inputs
 function sendEquation(){
     console.log('Equals button clicked.');
+    // POST
+    $.ajax({
+        method: 'POST',
+        url: '/history',
+        data: {
+            operandOne: $('#first-num').val(),
+            operandTwo: $('#second-num').val(),
+            operator: eqOperator
+        }
+    }).then(function(response){
+        console.log('Posting equation...', response);
+        getEquation();
+    }).catch(function(error){
+        alert('sendEquation failed', error);
+    })
+}
+
+// getEquation function to return result
+function getEquation(){
+    console.log('Getting equation results...');
+    // GET
+    $.ajax({
+        method: 'GET',
+        url: '/result'
+    }).then(function(response){
+        console.log('Getting result...', response);
+        render();
+    }).catch(function(error){
+        alert('getEquation failed:', error);
+    })
 }
 
 // function for DOM rendering
 function render(){
 
 }
-
 
 // clear user input fields, reset operator variable and reset operator button visibility
 function clearCalc(){
--- server/server.js
+++ server/server.js
@@ -6,6 +6,7 @@ const port = 5000;
 
 // declare/define global variables
 let history = require('./modules/history.js');
+let result = require('./modules/result.js');
 
 // serve up static files
 app.use(bodyParser.urlencoded({extended:true}));
@@ -20,17 +21,31 @@ app.post('/history', (req, res) => {
     console.log('Post request on /history');
     let thisEquation = req.body;
     history.push(thisEquation);
+    calculateResult();
     res.sendStatus(200);
 });
 
-// app.get('/history', (req, res) => {
-//     console.log('Request for /history made');
-//     res.send(history);
-// });
+app.get('/result', (req, res) => {
+    console.log('Request for /result made');
+    res.send(result);
+});
 
-// app.post('/history', (req, res) => {
-//     console.log('Post request on /history');
+// Function to calculate the result of a submitted equation
+function calculateResult(){
+    console.log('Calculating the result.');
+    // could use history[history.length-1]
+}
+
+// app.post('/result', (req, res) => {
+//     console.log('Post request on /result');
 //     let thisEquation = req.body;
 //     history.push(thisEquation);
 //     res.sendStatus(200);
 // });
+
+
+
+// Set up listening on port 5000
+app.listen(port, () => {
+    console.log('Server is running on port:', port);
+});
